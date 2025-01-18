3b4c981f43cdc8bbe19bb959b6c7b4541a6e8738
functionzz <jamie.hu.132@gmail.com>
2025-01-18 17:59:31 -0500

    extended search bar functionality

--- react-frontend/src/components/SearchBar.jsx
+++ react-frontend/src/components/SearchBar.jsx
@@ -3,14 +3,18 @@ import api from "../api";
 
 const SearchBar = () => {
   const [searchVal, setSearchVal] = useState("");
-  const [suggestions, setSuggestions] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [hideSuggestions, setHideSuggestions] = useState(true);
+
+  const [result, setResult] = useState(null);
 
   useEffect(() => {
     const res = async () => {
       try {
-        const { data } = await api.get("/api/search/?q=${searchVal}");
+        const { data } = await api.get('/api/search/', { params: { q: searchVal }});
 
         setSuggestions(data.results);
+        console.log(data.results);
       } catch (e) {
         alert(e);
       }
@@ -19,18 +23,45 @@ const SearchBar = () => {
     res();
   }, [searchVal]);
 
+  const findResult = (company_name) => {
+    setResult(suggestions.find((suggestion) => suggestion.company_name === company_name))
+  }
+
   return (
-    <>
+    <div className="flex-1 justify-center w-1/2">
       <input
         type="text"
         placeholder="Search for Companies..."
-         className="border border-gray-300 h-8 box-border p-[1px] px-2 outline-none transition duration-200 ease-in-out focus:border-blue-500"
+        className="border border-gray-200 h-8 outline-none box-border px-[10px] py-[1px] transition duration-200 ease-in-out w-full"
         value={searchVal}
         onChange={(e) => {
           setSearchVal(e.target.value);
         }}
+        onFocus={() => setHideSuggestions(false)}
+        onBlur={async () => {
+          setTimeout(() => {
+            setHideSuggestions(true);
+          }, 200);
+        }}
       ></input>
-    </>
+
+      <ul
+      className={`flex-1 justify-center menu dropdown-content bg-base-100 rounded-box z-[1] p-2 shadow ${
+        hideSuggestions ? "invisible" : "visible"}`}
+      >
+        {suggestions.map((suggestion) => (
+          <li
+            className="cursor-pointer box-border px-[10px] py-[1px] h-8 flex items-center hover:bg-slate-700"
+            key={suggestion.cik}
+
+            // useNavigate to specific company page
+            onClick={() => findResult(suggestion.company_name)}
+          >
+            {suggestion["ticker"]} - {suggestion["company_name"]}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
